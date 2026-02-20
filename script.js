d0c0b3aac67f84622bc94d88c7cebeb4002c2f73
vishalmorya888-ctrl <vishalmorya888@gmail.com>
2026-02-20 10:30:59 +0530

    javascript basis

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+// document.write("Hello World!");
+
+
+
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+num = false;
+console.log(num);
+console.log(typeof(num));
+num = "Hello World!";
+console.log(num);
+console.log(typeof(num));
+num = "hello";
+console.log(num);
+console.log(typeof(num));
+num = undefined;
+console.log(num);
+console.log(typeof(num));
+
+
+
+const sym1 = Symbol(4);
+console.log(sym1);
+const sym2 = Symbol(4);
+console.log(sym2);
+if(sym1===sym2){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+
+
+
+var a = 100; var b = 13; var c = 10; var linebreak = "<br>";
+document.write("a + b + c = ");
+result = a + b + c;
+document.write(result);
+document.write(linebreak);
