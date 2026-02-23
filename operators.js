a3ed807046814886d80e0dd648a4b9577b0e3654
vishalmorya888-ctrl <vishalmorya888@gmail.com>
2026-02-23 15:42:20 +0530

    html operators

---
+++ operators.js
@@ -0,0 +1,18 @@
+var a=10;
+var b=20;
+var linebreak="<br>";
+
+document.write("(a==b) : ");
+result = (a==b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a<b) : ");
+result = (a<b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a>b) : ");
+result = (a>b);
+document.write(result);
+document.write(linebreak);
