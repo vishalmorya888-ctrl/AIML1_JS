f22dde9cdd5a8c9aa148e9f800b71e1f7ec4400e
vishalmorya888-ctrl <vishalmorya888@gmail.com>
2026-02-27 10:28:02 +0530

    javascript object

--- object.js
+++ object.js
@@ -1,16 +1,28 @@
-var person = {
-    Name : "John Mac",
-    age : 30,
-    isMarried : true,
-    address : {
-        street : "vijay nagar",
-        flatNO : 201
-    }
-};
+// var person = {
+//     Name : "John Mac",
+//     age : 30,
+//     isMarried : true,
+//     address : {
+//         street : "vijay nagar",
+//         flatNO : 201
+//     }
+// };
 // console.log(person);
-console.log(person.Name);
-console.log(person.age);
-console.log(person.isMarried);
-console.log(person.address);
-console.log(person.address.street);
-console.log(person.address.flatNO); 
+// console.log(person.Name);
+// console.log(person.age);
+// console.log(person.isMarried);
+// console.log(person.address);
+// console.log(person.address.street);
+// console.log(person.address.flatNO); 
+
+
+//creating object using new keyword
+var person2 = new Object();
+person2.Name = "Krishnkumar";
+person2.age = 25;
+person2.isMarried = false;
+person2.address = {};
+person2.address.street = "Indrapuri";
+person2['address']['flatNO'] = 101;
+console.log(person2);
+
