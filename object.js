f3ce1ba54de9a10054ff547c5d1eeebb58d79679
vishalmorya888-ctrl <vishalmorya888@gmail.com>
2026-02-27 10:20:08 +0530

    javascript object

---
+++ object.js
@@ -0,0 +1,16 @@
+var person = {
+    Name : "John Mac",
+    age : 30,
+    isMarried : true,
+    address : {
+        street : "vijay nagar",
+        flatNO : 201
+    }
+};
+// console.log(person);
+console.log(person.Name);
+console.log(person.age);
+console.log(person.isMarried);
+console.log(person.address);
+console.log(person.address.street);
+console.log(person.address.flatNO); 
