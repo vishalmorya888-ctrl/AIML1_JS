a713b433239520c7e6645c60929cf939d0edf6a6
vishalmorya888-ctrl <vishalmorya888@gmail.com>
2026-02-27 10:07:03 +0530

    javascript functions

--- functions.js
+++ functions.js
@@ -1,5 +1,22 @@
-var res = mul(12,30);
-function mul(a,b){
-    return a*b;
+// var res = mul(12,30);
+// function mul(a,b){
+//     return a*b;
+// }
+// document.write("The result of multiplication is : " + res);
+
+
+//normal function
+function add(a,b){
+    console.log(a+b);
 }
-document.write("The result of multiplication is : " + res);
+add(10,20);
+
+//arrow function
+var add2 = (a,b) => {
+    console.log(a+b);
+};
+add2(130,40);
+
+//arrow function with single line parameter
+var add3 = (a,b) => console.log(a+b);
+add3(100,200);
