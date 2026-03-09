d16d5c9f28328a1d54c64041a3731bb768e97540
vishalmorya888-ctrl <vishalmorya888@gmail.com>
2026-03-09 15:55:33 +0530

    DOM operations

--- object.js
+++ object.js
@@ -1,19 +1,19 @@
-// var person = {
-//     Name : "John Mac",
-//     age : 30,
-//     isMarried : true,
-//     address : {
-//         street : "vijay nagar",
-//         flatNO : 201
-//     }
-// };
-// console.log(person);
-// console.log(person.Name);
-// console.log(person.age);
-// console.log(person.isMarried);
-// console.log(person.address);
-// console.log(person.address.street);
-// console.log(person.address.flatNO); 
+var person = {
+    Name : "John Mac",
+    age : 30,
+    isMarried : true,
+    address : {
+        street : "vijay nagar",
+        flatNO : 201
+    }
+};
+console.log(person);
+console.log(person.Name);
+console.log(person.age);
+console.log(person.isMarried);
+console.log(person.address);
+console.log(person.address.street);
+console.log(person.address.flatNO); 
 
 
 //creating object using new keyword
