89f582c6a01482df0e5ffd2bdc53dfcae1656e18
vishalmorya888-ctrl <vishalmorya888@gmail.com>
2026-03-09 15:54:08 +0530

    DOM operations

---
+++ Dom.js
@@ -0,0 +1,10 @@
+//Selecting with id
+const result = document.getElementById("Para");
+console.log(result);
+result.innerHTML = "my self";
+result.title = "myname";
+
+//Selecting with class
+const result1 = document.getElementsByClassName("heading");
+console.log(result1);
+result1.innerHTML = "hii";
