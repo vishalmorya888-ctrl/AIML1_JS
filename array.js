57fdbb5e51f4a4b61a83f48038cbc50c936a0c0a
vishalmorya888-ctrl <vishalmorya888@gmail.com>
2026-03-09 15:54:42 +0530

    array operations

---
+++ array.js
@@ -0,0 +1,21 @@
+var myInfo = new Array();
+var myInfo1 = Array();
+console.log(myInfo,myInfo1);
+
+
+var collection = [
+    10,
+    "Hello World!",
+    new String("abc"),
+    new Date()
+];
+console.log(collection);
+console.log(collection[0]);
+collection.teachername = "John Mac";
+collection.phonenumber = 1234567890;
+console.log(collection.teachername);
+console.log(collection.phonenumber);
+console.log(collection.length);
+console.log(collection);
+
+
