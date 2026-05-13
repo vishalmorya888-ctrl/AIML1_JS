0458c27752d5e9445d097928cb9b4802e7a87e2c
vishalmorya888-ctrl <vishalmorya888@gmail.com>
2026-05-13 15:22:44 +0530

    Attributes

--- attributes.js
+++ attributes.js
@@ -1,13 +1,14 @@
 //getAttribute() method
 
-const element = document.getElementById("myH1");
-let text = element.getAttribute("class");
-document.getElementById("demo").innerHTML = text;
+// const element = document.getElementById("myH1");
+// let text = element.getAttribute("class");
+// document.getElementById("demo").innerHTML = text;
 
 
 
 //setAttribute() method
 
-function myFunction() {
-    document.getElementById("myH1").setAttribute("class", "democlass");
-}
+// function myFunction() {
+//     document.getElementById("myH1").setAttribute("class", "democlass");
+// }
+
